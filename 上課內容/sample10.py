0b9c7b9cfb4b9f43a54a6460c890385d254d0c8f
roberthsu2003 <roberthsu2003@gmail.com>
2020-05-11 20:28:16 +0800

    modify

---
+++ 上課內容/sample10.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 20:01:57 2020
+
+@author: roberthsu2003
+"""
+
+a = 5
+
+if True:
+    print(a)
+    b = 10
+
+print(b)
+
+def userRun():
+    print('在userRun內取出b的值',b)
+    c = 10
+
+userRun()
+print(c)
---
+++ 上課內容/sample11.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 20:18:38 2020
+
+@author: roberthsu2003
+"""
+
+short_list = [1, 2, 3]
+position = 5
+
+try:
+    print(short_list[5])
+except:
+    print('發生錯誤')
