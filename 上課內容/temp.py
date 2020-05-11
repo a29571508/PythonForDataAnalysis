b79f4db8dad587da748e9e724a185b9a50fb4e62
roberthsu2003 <roberthsu2003@gmail.com>
2020-05-11 19:49:51 +0800

    modify

---
+++ 上課內容/temp.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a_set={number for number in range(1,6) if number % 3 ==1}
+print(a_set)
+
+mySet = {1, 2, 3, 4, 5, 6, 1, 2, 3}
+myList = list(mySet)
+print(myList[:3])
+
+
