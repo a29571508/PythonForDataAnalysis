8a8f557520687eccf085a6b9ed3838ef9a23bc13
roberthsu2003 <roberthsu2003@gmail.com>
2020-05-13 20:38:45 +0800

    modify

---
+++ 上課內容/modu.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def fun1():
+    print('這是modu模組內的fun1')
+    
+def fun2():
+    print('這是modu模組內的fun2')
+    
+def fun3():
+    print('這是modu模組內的fun3')
+
---
+++ 上課內容/sample12.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 20:31:54 2020
+
+@author: roberthsu2003
+"""
+
+import modu
+modu.fun1()
+modu.fun2()
+modu.fun3()
