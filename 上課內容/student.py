3e4e518a73dc2d0b4a0146ed2aab6bce5024d14b
roberthsu2003 <roberthsu2003@gmail.com>
2020-05-19 19:37:51 +0800

    modify

---
+++ 上課內容/student.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 19:25:41 2020
+
+@author: roberthsu2003
+"""
+
+class Student:
+    def __init__(self):
+        print("Student被建立了")
+        
