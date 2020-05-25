01447b3338dbf0e39c70ad3f387ddea9ea4956a8
roberthsu2003 <roberthsu2003@gmail.com>
2020-05-25 20:04:29 +0800

    modify

--- 上課內容/person.py
+++ 上課內容/person.py
@@ -5,9 +5,12 @@ Created on Mon May 25 19:22:50 2020
 
 @author: roberthsu2003
 """
-
+#module內的function
 def sayHello():
     print("sayHello!")
-
+    
+#module內的類別
 class Person:
-    pass;
+    #建立自訂的初始化
+    def __init__(self):
+        print("建立了一個實體")
